5685260f24a92ea4405657ff8478e84f43ec3dbc
tristan <tristan.xiery@gmail.com>
2015-05-04 21:53:46 +0800

    init my test repository

---
+++ antTest/src/tsp/HelloAnt.java
@@ -0,0 +1,18 @@
+package tsp;
+
+import org.json.JSONException;  
+import org.json.JSONObject;
+
+public class HelloAnt {
+	public static void main(String[] args) {
+		System.out.println("hello ant");
+		String dataString = "{\"name\":\"tristan\",\"properties\":{\"a\":\"a\",\"b\":\"b\"}}";
+
+		try {
+            JSONObject dataJson = new JSONObject(dataString);
+            System.out.println(dataJson.toString());
+		} catch (JSONException e) {
+            System.out.println(e);
+		}
+	}
+}
